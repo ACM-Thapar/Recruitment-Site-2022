fdd4a89703e90b3f211ecd19c6a51bd6157dee0b
Jogesh Gupta <jogeshgupta963@gmail.com>
2022-07-12 18:40:27 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { Fragment } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Content from './Components/Content/Content'
 import Footer from './Components/Footer/Footer'
-import RecruitmentForm from './Components/Form/Form'
+import RecruitmentForm from './Components/RecruitmentForm/RecruitmentForm'
 import { Routes, Route } from 'react-router-dom'
-
+import './App.css'
 function App() {
   return (
     <Fragment>
